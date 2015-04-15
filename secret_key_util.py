4311ac95f51b9dd2c37e92f9df2767927fc898f5
fhanspach <f.hanspach@its-bh.de>
2015-04-15 12:59:03 +0200

    moved config to constants, read and write the secret key from file to support multiple workers

--- secret_key_util.py
+++ secret_key_util.py
@@ -1,6 +1,11 @@
 import os
 import random
 
+SECRET_KEY_FILE_PATH = os.path.expanduser("~/.secret_key")
+SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+MSG_KEY_NOT_SET = "SECRET_KEY was not set. Generated a new Secret Key."
+
 
 def get_secret_key():
     """
@@ -9,7 +14,16 @@ def get_secret_key():
     """
     key = os.environ.get("SECRET_KEY", None)
     if key is None:
-        print("SECRET_KEY was not set. Generated a new Secret Key.")
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        if os.path.isfile(SECRET_KEY_FILE_PATH):
+            # file exists, read the key
+            with open(SECRET_KEY_FILE_PATH, "r") as secret_key_file:
+                key = secret_key_file.read()
+                return str(key)
+
+        # the key isn't set yet - a new one will be created
+        print(MSG_KEY_NOT_SET)
+        chars = SECRET_KEY_CHARS
         key = ''.join(random.choice(chars) for _ in range(50))
+        with open(SECRET_KEY_FILE_PATH, "w+") as secret_key_file:
+            secret_key_file.write(key)
     return str(key)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='secret_key_util',
-    version='1.0',
+    version='1.1',
     maintainer='Felix Hanspach',
     maintainer_email="f.hanspach@cuescience.de",
 )
