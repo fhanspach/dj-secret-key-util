f7aa09e171ad20037e382f6a2e5663ca093e530c
fhanspach <f.hanspach@cuescience.de>
2016-02-29 12:06:30 +0100

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='secret_key_util',
-    version='1.2',
+    version='1.3',
     maintainer='Felix Hanspach',
     maintainer_email="f.hanspach@cuescience.de",
 )
