ed3a4ba461784ece93068e2ab62a8d6270b7a601
fhanspach <f.hanspach@cuescience.de>
2016-03-10 15:18:58 +0100

    refactored version to a separate file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
-
+from __version__ import VERSION
 setup(
     name='secret_key_util',
-    version='1.2.3',
+    version= VERSION,
     maintainer='Felix Hanspach',
     maintainer_email="f.hanspach@cuescience.de",
 )
